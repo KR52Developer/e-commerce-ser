771f804fcbed78f53af5fcbc43e53c5b3f39e379
Kavin R <127727609+KR52Developer@users.noreply.github.com>
2023-11-07 17:52:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
-// app.use(express.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
 
@@ -41,13 +40,7 @@ app.get("/", (req, res) => {
     res.status(200).send("Welcome to Ecommerce Server");
 });
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running in https:/localhost:${PORT}`);
-// });
 
-
-// mongoose.connect(process.env.DB_HOST);
-// console.log(process.env.DB_HOST);
 
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, UseUnifiedTopology: true })
